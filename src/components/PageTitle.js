f9966ba89524c8ee3679c714a8c4ac8c1fdd3aac
29Shreyas <sihasaneshreyas@gmail.com>
2022-09-16 22:12:20 +0530

    Todo_App

---
+++ src/components/PageTitle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from '../styles/modules/title.module.scss';
+
+function PageTitle() {
+  return <p className={style.title}>TODO LIST</p>;
+}
+
+export default PageTitle;
